2d7621f7a53f6cf2fd3915ba7dd61760a6100293
MedAhmedKrichen <57727551+MedAhmedKrichen@users.noreply.github.com>
2020-03-06 23:06:43 +0100

    Create eduonix project1.py

---
+++ eduonix project1.py
@@ -0,0 +1,25 @@
+
+target=10
+games=int(input("give the number of games you wanna to play"))
+for i in range(games):
+    k=0
+    s = int(input("give a random  number between 1-25"))
+
+    while s not in(range(1,26)):
+            s=int(input("give a random  number between 1-25"))
+    while s != target:
+        if s>target :
+            print("the number is higher")
+            k+=1
+            s = int(input("give a random  number between 1-25"))
+            while s not in (range(1, 26)):
+                s = int(input("give a random  number between 1-25"))
+        elif s < target:
+            print("the number is lower")
+            k +=1
+            s = int(input("give a random  number between 1-25"))
+            while s not in (range(1, 26)):
+                s = int(input("give a random  number between 1-25"))
+    else :
+        k +=1
+        print("you guess it right after %d"%k)
